4d8816be0a75273a9e563016a6af2fda4ee77036
Elliott Porter Jr <elliott.porter@ipsoft.com>
2019-03-25 23:25:03 -0400

    Initial commit for Kotlin-Classes

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val elliott = Player("elliott")
+    println(elliott.name)
+    println(elliott.lives)
+    println(elliott.level)
+    println(elliott.score)
+}
---
+++ src/main/kotlin/Player.kt
@@ -0,0 +1,2 @@
+class Player(var name: String, var lives: Int = 3, var level: Int = 1, var score: Int = 0) {
+}
