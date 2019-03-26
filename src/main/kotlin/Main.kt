b24a017b41d3b6e5ff2469ccd24b0f9d8c1c9644
Elliott Porter Jr <elliott.porter@ipsoft.com>
2019-03-26 06:07:30 -0400

    Using function from Player class to display Player attributes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
 fun main(args: Array<String>) {
     val elliott = Player("elliott")
-    println(elliott.name)
-    println(elliott.lives)
-    println(elliott.level)
-    println(elliott.score)
+
+    elliott.show()
 }
--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -1,2 +1,10 @@
 class Player(var name: String, var lives: Int = 3, var level: Int = 1, var score: Int = 0) {
+    fun show() {
+        println("""
+            $name
+            $lives
+            $level
+            $score
+        """.trimIndent())
+    }
 }
